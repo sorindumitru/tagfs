44f5147a424f029c0fff6f7a6b269eb394a5998d
Catalina Macalet <catalina.macalet@gmail.com>
2011-01-09 16:52:53 +0200

    user tool for tag manipulation

--- src/include/tag_interface.h
+++ src/include/tag_interface.h
@@ -6,23 +6,115 @@
 #ifndef TAG_INTERFACE_H
 #define TAG_INTERFACE_H
 
+#include <stdio.h>
+#include <errno.h>
+
 
 #define MAX_TAG_COUNT 256
-#define MAX_TAG_LENGTH 245
+#define MAX_TAG_LENGTH 16
+
+#define F_TAG_ADD   17 /*TagFS*/
+#define F_TAG_DEL   18
+#define F_TAG_LIST  19
+#define F_TAG_CREAT_EP 20
+
+struct tagfs_ops{
+    unsigned str_len;
+    char *str;
+};
+
+struct tagfs_list{
+    unsigned fname_size;
+    char *filename;
+    char *tags;
+};
+
 /*
 Returns an array of tags for a specific file.
 Tag_count  parameter is being set to the number of tags by called.
 */
-char ** get_tags(char *filename /* directory ? */, int *tag_count);
+char ** get_tags(char *filename /* directory ? */, int *tag_count)
+{
+    int fd = open("/tmp/tagfs",O_CREAT);
+    struct tagfs_list arg;
+    arg.fname_size = strlen(filename);
+    arg.filename = calloc(strlen(filename),sizeof(char));
+    memcpy(arg.filename,filename,strlen(filename));
+    arg.tags = calloc(MAX_TAG_COUNT * MAX_TAG_LENGTH,sizeof(char));
+     
+    int count = fcntl(fd, F_TAG_LIST,&arg);
+    if ( count == -1 ) { /*TODO: see how arg should look like*/
+        perror("Error occured at get_tafs");
+        close(fd);
+        return NULL;
+    }
+    *tag_count = count;
+    close(fd);
+    //tags_matrix
+    {
+        char **matrix = calloc(count, sizeof(char*));
+        char *ctag = strtok(arg.tags,":");
+        int i=0;
+        while (ctag != NULL){
+            matrix[i] = calloc(strlen(ctag),sizeof(char));
+            memcpy(matrix[i],ctag,strlen(ctag));
+            i++;
+            ctag = strtok(NULL, ":");
+        }
+    free(arg.filename);
+    free(arg.tags);
+    return matrix;
+    }
+}
+
 /*
 Adds the given tag as a tag for specified file
 Returns 1 if failure, 0 if success.
 */
-int add_tag_to_file(char *filename /*directory?*/, char * tag);
+int add_tag_to_file(char *str)
+{
+    int fd = open("/tmp/tagfs",O_CREAT);
+    struct tagfs_ops arg;
+    arg.str_len = strlen(str);
+    arg.str = calloc(strlen(str),sizeof(char));
+    memcpy(arg.str,str,strlen(str));
+    if ( fcntl(fd, F_TAG_ADD,&arg) == -1 ) {
+        close(fd);
+        return 1;
+    }
+    close(fd);
+    free(arg.str);
+    return 0;
+}
+
 /*
 Removes the given tag as a tag for specified file
 Returns 1 if failure, 0 if success.
 */
-int remove_tag_from_file(char *filename /*directory?*/, char * tag);
+int remove_tag_from_file(char *str)
+{
+    int fd = open("/tmp/tagfs",O_CREAT);
+    struct tagfs_ops arg;
+    arg.str_len = strlen(str);
+    arg.str = calloc(strlen(str),sizeof(char));
+    memcpy(arg.str,str,strlen(str));
+    if ( fcntl(fd, F_TAG_DEL,&arg) ==-1 ) {
+        close(fd);
+        return 1;
+    }
+    close(fd);
+    free(arg.str);
+    return 0;
+}
 
+int create_tag_entry()
+{
+    int fd = open("/tmp/tagfs",O_CREAT);
+    if ( fcntl(fd, F_TAG_CREAT_EP) ==-1 ) { 
+        close(fd);
+        return 1;
+    }
+    close(fd);
+    return 0;
+}
 #endif
--- src/user/tag.c
+++ src/user/tag.c
@@ -4,65 +4,119 @@
 	File Main Owner: Eugen Hristev
 ***************/
 
-#include "stdio.h"
-#include "string.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include "../include/tag_interface.h"
 
+//new commands
+#define COMMANDS    4
+char *args[] = {"-c","-a","-d","-l"}; 
+
+int invalid_arg( char *cmd )
+{
+    int i;
+    for ( i=0; i < COMMANDS; i++)
+        if( !strcmp(cmd,args[i]) )
+            return 0;
+    printf("invalid arg=%s\n",cmd);
+    return 1;
+}
+
+
 int main(int argc, char * argv[])
 {
-	if( argc < 3)
+	if( argc < 2 || argc > 3 || invalid_arg(argv[1]) )
 	{
-		printf("Usage: tag (-a/-d/-l) filename [tag]\n");
+		printf("Usage: tag (-a/-d/-l) filename[:tag1:...:tagn]\nOR\ntag -c\n");
+		printf("argc = %d\n",argc);
+        return 1;
+	}
+
+    if ( argc == 3 && strcmp(argv[1],"-c") ){
+        
+        //filename existence check
+        char *arg = calloc(strlen(argv[2]), sizeof(char));
+        memcpy(arg,argv[2],strlen(argv[2]));
+        char *filename = strtok(arg,":");
+        FILE *infile = fopen(filename,"rt");
+                
+        if(!infile)
+        {
+            printf("Invalid file !\n");
+            return 1;
+        }
+        free(arg);
+
+        if(!strcmp(argv[1],"-l"))
+        {
+            if ( strchr(argv[2],':') != NULL ){
+                printf("invalid argument at %s ,not expected <:>",argv[1]);
+                return 1;
+            }
+
+            char **tags;
+            int count,i;
+            
+            printf("Tags :\n");
+            tags = get_tags(argv[2],&count);
+            if (tags == NULL){
+                printf("Could not get taglist");
+                return 1;
+            }
+            for(i=0;i<count;i++)
+                printf("%s\n",tags[i]);
+                
+            for(i=0;i<count;i++)
+                free(tags[i]);
+            free(tags);
+        }
+        
+        if(!strcmp(argv[1],"-a"))
+        {
+            if( strchr(argv[2],':') == NULL )
+            {
+                printf("No tag specified !\n");
+                return 1;
+            }
+            if( !add_tag_to_file(argv[2]) )
+                printf("Successfully added tag to file !\n");
+            else 
+                printf("Could not add tag to file !\n");
+        }
+        
+        if(!strcmp(argv[1],"-d"))
+        {
+            if( strchr(argv[2],':') == NULL )
+            {
+                printf("No tag specified !\n");
+                return 1;
+            }
+
+            if( !remove_tag_from_file(argv[2]))
+                printf("Successfully deleted tag from file !\n");
+            else 
+                printf("Could not delete tag from file !\n");
+        }
+	}else if ( strcmp(argv[1],"-c") ){
+		printf("Usage: tag (-a/-d/-l) filename[:tag1:...:tagn]\n OR\n tag -c\n");
 		return 1;
-	}
-	FILE *infile = fopen(argv[3],"rt");
-		
-	if(!infile)
-	{
-		printf("Invalid file !\n");
-		return 1;
-	}
-	if(!strcmp(argv[1],"-l"))
-	{
-		char tags[MAX_TAG_COUNT][MAX_TAG_LENGTH];
-		char **tagsp=(char **)tags;
-		int count,i;
-		
-		printf("Tags :\n");
-		tagsp = get_tags(argv[3],&count);
-		for(i=0;i<count;i++)
-			printf("%s\n",tags[i]);
-			
-	}
-	
-	if(!strcmp(argv[1],"-a"))
-	{
-		if(argc<4)
-		{
-			printf("No tag specified !\n");
-			return 1;
-		}
-		if( add_tag_to_file(argv[3], argv[4]))
-			printf("Successfully added tag to file !\n");
-		else 
-			printf("Could not add tag to file !\n");
-	}
-	
-	if(!strcmp(argv[1],"-d"))
-	{
-		if(argc<4)
-		{
-			printf("No tag specified !\n");
-			return 1;
-		}
-		if( remove_tag_from_file(argv[3], argv[4]))
-			printf("Successfully deleted tag from file !\n");
-		else 
-			printf("Could not deelte tag from file !\n");
-	}
-	
-		
+    }else{
+        if ( strcmp(argv[1],"-c") ){
+		    printf("Usage: tag (-a/-d/-l) filename[:tag1:...:tagn]\n OR\n tag -c\n");
+		    return 1;
+        }
+        //create start point
+        if ( !create_tag_entry() )
+           printf("Created new entry point for TagFS\n");
+        else
+           printf("Could not create new entry point for TagFS\n"); 
+    }
 
 
 	return 0;
